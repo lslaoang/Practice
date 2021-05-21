45b20d7e95209efd14acfb5278574d4f8b90d7fb
Laurel Lao Ang <laurellaoang@gmail.com>
2021-05-21 23:43:39 +0800

    Added FrogJmp

---
+++ src/missingElement.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class missingElement {
+
+    private int missingNumber(int[] A){
+
+        if(A.length>0){
+            int next =1;
+            int init = A[0];
+            int arrLen = A.length;
+
+           if(A[0]!=0) {
+               for (int i = 1; i < arrLen; i++) {
+                   init = init ^ A[i];
+               }
+               for (int i = 2; i <= arrLen + 1; i++){
+                   next = next ^ i;
+                   System.out.println(next);
+               }
+           }
+
+        return (init ^ next);
+        }
+    return 1;
+    }
+
+    public static void main(String[]args){
+        missingElement miss = new missingElement();
+
+       System.out.println(miss.missingNumber(new int[] {1,2,3,5}));
+       //System.out.println(miss.missingNumber(new int[] {1,2}));
+       //System.out.println(miss.missingNumber(new int[] {3,2}));
+       //System.out.println(miss.missingNumber(new int[] {2}));
+       //System.out.println(miss.missingNumber(new int[] {}));
+
+    }
+
+}
