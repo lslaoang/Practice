199e841e6c7d17aa5a7b071d529ed07cea23d21e
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-21 14:55:51 +0800

    use XOR operator

---
+++ src/SumOfNumber.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SumOfNumber {
+
+    private List<Integer> addendsOf(int num){
+
+        Set<Integer> setOfNumber = new HashSet<>();
+        setOfNumber.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+
+
+        int delta = 0;
+        for(int i=1;i<=10;i++){
+            if(setOfNumber.contains(i)){
+
+            }
+        }
+
+        return Arrays.asList(-1,-1);
+    }
+
+
+    public static void main(String args[]){
+
+    }
+}
--- src/missingElement.java
+++ src/missingElement.java
@@ -1,8 +1,8 @@
-import java.util.Arrays;
-
 public class missingElement {
 
     private int missingNumber(int[] A){
+
+        //Using XOR operator
 
         if(A.length>0){
             int next =1;
@@ -11,7 +11,7 @@ public class missingElement {
 
            if(A[0]!=0) {
                for (int i = 1; i < arrLen; i++) {
-                   init = init ^ A[i];
+                  init = init ^ A[i];
                }
                for (int i = 2; i <= arrLen + 1; i++){
                    next = next ^ i;
@@ -26,7 +26,7 @@ public class missingElement {
     public static void main(String[]args){
         missingElement miss = new missingElement();
 
-       System.out.println(miss.missingNumber(new int[] {1,2,3,0}));
+       System.out.println(miss.missingNumber(new int[] {1,2,3,5,7,4}));
        //System.out.println(miss.missingNumber(new int[] {1,2}));
        //System.out.println(miss.missingNumber(new int[] {3,2}));
        //System.out.println(miss.missingNumber(new int[] {2}));
