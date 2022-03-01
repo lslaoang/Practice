6f3afc35f00e61568199f3bef7a68f302b9d2574
Laurel Lao Ang <laurellaoang@gmail.com>
2022-03-01 12:15:52 +0800

    Duplicates

--- src/FindTheDuplicate.java
+++ src/FindTheDuplicate.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FindTheDuplicate {
     private static int findDuplicate(int[] nums) {
@@ -24,26 +26,21 @@ public class FindTheDuplicate {
         return fast;
     }
 
-    public static int findDuplicateByBrute(int ... num){
+    public static Set<Integer> findDuplicateByBrute(int ... num){
+        Set<Integer> duplicates = new HashSet<>();
         Arrays.sort(num);
         for(int i = 0; i < num.length; i++){
             for(int x = 0; x < i; x++){
                 if(num[x] == num[i]){
-                    return num[x];
+                    duplicates.add(num[x]);
                 }
             }
         }
 
-        return 1;
+        return duplicates;
     }
     public static void main(String[] args) {
-        // Example 1
-        int nums[] = new int[]{1, 3, 4, 4,4, 2};
-      //  System.out.println(findDuplicate(nums));
-        // Example 2
-       // nums = new int[]{3, 1, 5, 4, 2, 5};
-       // System.out.println(findDuplicate(nums));
-
-        System.out.println(findDuplicateByBrute(nums));
+        System.out.println(findDuplicateByBrute(1,1,3, 3, 4, 4,4, 2));
+        System.out.println(findDuplicateByBrute(10, 3, 2, 4,4, 2));
     }
 }
