16f3be97c471ab613658178072357206db7dbfe7
Laurel Laoang, Jr <laurellaoang@gmail.com>
2022-03-06 11:24:27 +0800

    Update ChangeDomain.java

--- src/ChangeDomain.java
+++ src/ChangeDomain.java
@@ -1,14 +1,14 @@
 public class ChangeDomain {
 
-    public static String changeDomain(String email){
+    public static String changeDomainToDefault(String email){
         return email.substring(0,email.indexOf("@")) + "@ubs.com";
     }
 
     public static void main (String[] args){
-        System.out.println(changeDomain("laurel.laoang@email.com"));
-        System.out.println(changeDomain("laurfsael.laoang@s.com"));
-        System.out.println(changeDomain("laurrel.laoang@fsf.com"));
-        System.out.println(changeDomain("lauressl.laoang@ss.com"));
+        System.out.println(changeDomainToDefault("laurel.laoang@email.com"));
+        System.out.println(changeDomainToDefault("laurfsael.laoang@s.com"));
+        System.out.println(changeDomainToDefault("laurrel.laoang@fsf.com"));
+        System.out.println(changeDomainToDefault("lauressl.laoang@ss.com"));
 
     }
 }
