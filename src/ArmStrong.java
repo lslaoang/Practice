ca43b0c8b3c9789d7bc947189fc8ad68c15c5022
Laurel Lao Ang <laurellaoang@gmail.com>
2022-03-14 14:23:23 +0800

    Updated Armstrong implementation

--- src/ArmStrong.java
+++ src/ArmStrong.java
@@ -1,31 +1,42 @@
+/**
+ * Armstrong number is a number which will result to the same figure if you multiply each individual digits.
+ * Example : 153 --> 1 x 5 x 3 = 153
+ */
 public class ArmStrong {
 
     public boolean isArmString(int num){
         int initialValue = num;
-        int sum = 0;
-        int remainder;
-        boolean flag = false;
+        int sum = 0, remainder;
+        int digits = numberOfDigits(num);
+
 
         while(initialValue > 0){
             remainder = initialValue % 10;
-            sum += (remainder * remainder * remainder);
+            sum += Math.pow(remainder, digits);
             initialValue = initialValue / 10;
         }
-        if(sum == num){
-            flag = true;
+        return sum == num;
+    }
+    private int numberOfDigits(int number) {
+        int counter = 0;
+        while(number != 0){
+            counter++;
+            number = number / 10;
         }
-        return flag;
+        /* Returned number should be greater than 1. The minimum operation should be squared of num */
+        return counter == 1 ? 2 : counter;
     }
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         ArmStrong armStrong = new ArmStrong();
-        System.out.println(armStrong.isArmString(153));
-        System.out.println(armStrong.isArmString(370));
-        System.out.println(armStrong.isArmString(371));
-        System.out.println(armStrong.isArmString(407));
-        System.out.println(armStrong.isArmString(200));
-        System.out.println(armStrong.isArmString(1));
-        System.out.println(armStrong.isArmString(0));
-        System.out.println(armStrong.isArmString(3));
+        System.out.println("Is 153 armstrong? " + armStrong.isArmString(153));
+        System.out.println("Is 370 armstrong? " +armStrong.isArmString(370));
+        System.out.println("Is 371 armstrong? " +armStrong.isArmString(371));
+        System.out.println("Is 407 armstrong? " +armStrong.isArmString(407));
+        System.out.println("Is 200 armstrong? " +armStrong.isArmString(200));
+        System.out.println("Is 1 armstrong? " +armStrong.isArmString(1));
+        System.out.println("Is 0 armstrong? " +armStrong.isArmString(0));
+        System.out.println("Is 3 armstrong? " +armStrong.isArmString(3));
+        System.out.println("Is 1634 armstrong? " +armStrong.isArmString(1634));
     }
 }
