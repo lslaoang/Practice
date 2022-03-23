e2ec6f7dc9d11760253b919e5e0f9c736b6b8c4b
Laurel Lao Ang <laurellaoang@gmail.com>
2022-03-23 11:23:26 +0800

    Implementation updated for pop and push

--- src/StackVector.java
+++ src/StackVector.java
@@ -1,30 +1,33 @@
 import java.util.Vector;
 
 public class StackVector {
-    private static Vector<Integer> stack;
+    private static Vector<Integer> stack = new Vector<>();
 
-    StackVector() {}
-
- public void push(int num){
-        stack.add(num);
-        System.out.printf("Number %s added to the stack.", num);
- }
-
-    public void pop(){
-        int lastElement = stack.lastElement();
-        stack.removeElementAt(lastElement);
-        System.out.printf("Number %d removed from the stack.", lastElement);
-    }
-
-    public void popAll(){
-        stack.removeAllElements();
-        System.out.println("Removed all elements in the stack.");
-    }
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         StackVector stackVector = new StackVector();
         stackVector.push(1);
-        stackVector.push(1);
-        stackVector.push(1);
+        stackVector.push(2);
+        stackVector.push(3);
+
+        stackVector.pop();
+        stackVector.pop();
+
+        stackVector.popAll();
+    }
+
+    public void push(int num) {
+        stack.add(num);
+        System.out.printf("Number %s added to the stack.\n", num);
+    }
+
+    public void pop() {
+        int lastElement = stack.lastElement();
+        stack.removeElementAt(lastElement - 1);
+        System.out.printf("Number %d removed from the stack.\n", lastElement);
+    }
+
+    public void popAll() {
+        stack.removeAllElements();
+        System.out.println("Removed all elements in the stack.\n");
     }
 }
