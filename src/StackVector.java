c7ac33b326c01f8bd727387e788fd30ad522de91
Laurel Lao Ang <laurellaoang@gmail.com>
2022-03-23 11:43:39 +0800

    Added documentation

--- src/StackVector.java
+++ src/StackVector.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
+/**
+ * Using vector to avoid size declaration and Runtime exceptions
+ */
 public class StackVector {
     private static Vector<Integer> stack = new Vector<>();
 
