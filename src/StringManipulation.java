3ca6d560e804b0b05f64336247d1b4db9f565b01
Laurel Lao Ang <laurellaoang@gmail.com>
2022-03-24 10:52:04 +0800

    String.replace

--- src/StringManipulation.java
+++ src/StringManipulation.java
@@ -1,22 +1,27 @@
 public class StringManipulation {
 
-    private String removeChar(String word, char index){
+    private String removeChar(String word, char letter){
 
-        int charLocation = word.indexOf(index);
         char[] nWord = word.toCharArray();
 
         StringBuilder sb = new StringBuilder();
         for(char c : nWord){
-            if ((c != index))   sb.append(c);
+            if ((c != letter))   sb.append(c);
         }
-        String resultWord = sb.toString();
-        return resultWord;
+        return sb.toString();
+    }
+
+    private static String removeWord(String sentence, String word){
+            return sentence.replace(word,"");
     }
 
     public static void main(String[]args){
-            String thisword ="The quick brown fox jumps over the lazy dog";
+            String thisWord ="The quick brown fox jumps over the lazy dog";
             StringManipulation sm = new StringManipulation();
             System.out.println(sm.removeChar("The quick brown fox jumps over the lazy dog",'c'));
-            System.out.println(thisword.replace("c",""));
+            System.out.println(sm.removeChar("This is sample of word",'e'));
+            System.out.println(thisWord.replace("c",""));
+
+            System.out.println(removeWord("What sentence is this?", "is "));
     }
 }
