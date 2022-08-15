f69a88c9541d8aee289a056cdea0f702b7104149
Laurel Lao Ang <laurellaoang@gmail.com>
2022-08-15 14:04:02 +0800

    hashmap impl

--- src/GetString.java
+++ src/GetString.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,9 +12,18 @@ public class GetString {
         return Arrays.stream(numbersOnly.split(",")).collect(Collectors.toList());
     }
 
+    public static HashMap<String, Integer> getVersion(String version) {
+        List<String> versionArray = Arrays.stream(version.split("\\.")).toList();
+        HashMap<String, Integer> validVersion = new HashMap<>();
+        validVersion.put("MAJOR", Integer.parseInt(versionArray.get(0)));
+        validVersion.put("MINOR", Integer.parseInt(versionArray.get(1)));
+        return validVersion;
+    }
+
     public static void main(String[] args) {
         final List<String> stats = readInput();
-        System.out.println(Arrays.toString(stats.toArray()));
+//        System.out.println(Arrays.toString(stats.toArray()));
+        System.out.println(getVersion("111.222.33"));
 
     }
 }
